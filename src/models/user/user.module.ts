6025fdfd27c441c75148040a522f1060d6ad4172
Gabriel Monteiro Ricardo <gmricardo@viptech.com.br>
2024-11-21 12:20:30 -0300

    configuring user module, adding userService e typeOrm for feature

--- src/models/user/user.module.ts
+++ src/models/user/user.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
 import { isUniqueEmailValidator } from 'src/models/user/validators/isUniqueEmail.validator';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './user.entity';
+import { UserService } from './user.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
-  providers: [UserRepository, isUniqueEmailValidator],
+  providers: [UserService, UserRepository, isUniqueEmailValidator],
 })
 export class UserModule {}
