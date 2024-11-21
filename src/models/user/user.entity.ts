49d545198c09bca1f18b986e842a4560943c2697
Gabriel Monteiro Ricardo <gmricardo@viptech.com.br>
2024-11-21 10:53:46 -0300

    addding auto increment to uuid

--- src/models/user/user.entity.ts
+++ src/models/user/user.entity.ts
@@ -3,11 +3,13 @@ import {
   CreateDateColumn,
   DeleteDateColumn,
   Entity,
+  PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 
 @Entity({ name: 'users' })
 export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column({ name: 'name', length: 100, nullable: false })
