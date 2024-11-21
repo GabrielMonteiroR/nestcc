8787aeee432c33912f2911d8b089448a6ec76628
Gabriel Monteiro Ricardo <gmricardo@viptech.com.br>
2024-11-21 10:56:59 -0300

    making db recognize entities

--- src/config/postgres.config.service.ts
+++ src/config/postgres.config.service.ts
@@ -13,7 +13,7 @@ export class PostgresConfigService implements TypeOrmOptionsFactory {
       username: this.configService.get<string>('DB_USERNAME'),
       password: this.configService.get<string>('DB_PASSWORD'),
       database: this.configService.get<string>('DB_NAME'),
-      entities: [],
+      entities: [__dirname + '/**/src/models/*.entity{.js,.ts}'],
       synchronize: true,
     };
   }
