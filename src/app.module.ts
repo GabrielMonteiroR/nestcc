a554c8fc674e9656936146ed40fcbbdef6633e7f
Gabriel Monteiro Ricardo <gmricardo@viptech.com.br>
2024-11-21 09:17:05 -0300

    module config for typeOrm and postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { SportsComplexModule } from './sports_complex/sportsComplex.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    SportsComplexModule,
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
 })
 export class AppModule {}
